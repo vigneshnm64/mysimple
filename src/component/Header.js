8c65db55bd609308f8d4bddde07f1777618cff45
vigneshnm64 <vigneshnm64@gmail.com>
2023-07-07 11:16:18 +0530

    navbarcommit

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,9 +1,14 @@
 import React from 'react'
-
+import './Header.css'
 const Header = () => {
   return (
-    <div>
-      <p>go to jeswin</p>
+    <div >
+    <div class="topnav">
+    <a class="active" href="#home">Home</a>
+    <a href="#news">News</a>
+    <a href="#contact">Contact</a>
+    <a href="#about">About page</a>
+  </div>
     </div>
   )
 }
