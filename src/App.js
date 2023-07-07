648af8b25a65f18f2050747c9a7ec535d3b1bed1
vigneshnm64 <vigneshnm64@gmail.com>
2023-07-07 11:11:56 +0530

    newchangeappjs

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react'
-
+import Header from './component/Header'
 const App = () => {
   return (
     <div>
+    <div>
+    <Header />
+    </div>
       <p>Hello Kiran Sir doing well</p>
       <p> hi kiran sir doing well</p>
     </div>
