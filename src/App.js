dffc2bd8b6295334b0d33d7bdfd7235973b9bcd3
vigneshnm64 <vigneshnm64@gmail.com>
2023-07-07 11:25:05 +0530

    new2commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from 'react'
 import Header from './component/Header'
+import Footer from './component/Footer'
 const App = () => {
   return (
     <div>
+    
     <div>
     <Header />
     </div>
       <p>Hello Kiran Sir doing well</p>
       <p> hi kiran sir doing well</p>
+      <div>
+      <Footer />
+      </div>
     </div>
   )
 }
---
+++ src/component/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+const Header = () => {
+  return (
+    <div >
+    <div class="topnav">
+    <a class="active" href="#home">Home</a>
+    <a href="#news">News</a>
+    <a href="#contact">Contact</a>
+    <a href="#about">About page</a>
+  </div>
+    </div>
+  )
+}
+
+export default Header
