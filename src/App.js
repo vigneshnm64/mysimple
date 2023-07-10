19162fb9e7f360242f66e55050ea307a41b243ef
vigneshnm64 <vigneshnm64@gmail.com>
2023-07-10 08:45:36 +0530

    the submit

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
                 Name of<input type="text" />
                 Email<input type="email" />
                 Password for<input type="password " />
-                Subbmite<input type={"submmitted"}
+                Subbmitee<input type={"submmitted"}
                     style={{ backgroundColor: "#a1eafb" }} />
             </form>
             <div>
